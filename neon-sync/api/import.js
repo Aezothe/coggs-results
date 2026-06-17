85fdc291784b2a01438cb46f75aef341a2a35bb7
Aezothe <rutger.van.huber@gmail.com>
2026-06-17 18:22:23 -0500

    Testing if Vercel can handle this

--- neon-sync/api/import.js
+++ neon-sync/api/import.js
@@ -1,6 +1,10 @@
 import { runImport } from '../src/import-entries.js'
 
 export default async function handler(req, res) {
+  return res.json({ ok: true, logs: ['test log'] })
+}
+
+export default async function handler(req, res) {
   const token = req.headers['x-admin-token']
 
   if (token !== process.env.ADMIN_SECRET) {
