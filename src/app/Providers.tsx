f7dca0a81ab70c5435e4b5b3ef44d94057fa92a8
Aezothe <rutger.van.huber@gmail.com>
2026-06-29 15:56:15 -0500

    Removing loading bar.

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export function Providers() {
   return (
     <>
-      <ProgressBar
-        height="3px"
-        color="#2563eb"
-        options={{ showSpinner: false }}
-        shallowRouting
-      />
       <Analytics />
       <SpeedInsights />
     </>
